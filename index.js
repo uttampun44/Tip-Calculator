2eb01b650d2c8f9ae4aeacee42a5343c83faf103
uttam pun <uttampun62@gmail.com>
2023-08-08 21:43:53 +0545

    tip calculator project  finish

--- index.js
+++ index.js
@@ -1,62 +1,63 @@
 let checkbill = document.getElementById('checkbill');
+let perperson = document.getElementById('perpersonbill');
 
-let increment = document.getElementById('increment');
-let decrement = document.getElementById('decrement');
+let totalbill;
+
+let dollar;
 
 // total bill
 function totabillresturant () {
     let total = document.getElementById('totalbill').value;
     const resturanttax = document.getElementById('resturanttax').value;
-      
+
     let tax = Number((total * resturanttax)/100);
 
-    let totalbill = parseInt(total) + parseInt(tax);
+    totalbill = parseInt(total) +  parseInt(tax);
 
     //  currency format
-    const dollar = new Intl.NumberFormat('en-us', {
+    dollar = new Intl.NumberFormat('en-us', {
      currency: 'USD',
      style: 'currency'
     })
 
-    alert(dollar.format(`${totalbill}`));
+    let invoice = document.querySelector('#total')
 
-    console.log(dollar.format(totalbill))
- 
-    let ptag = document.createElement('p');
-
-    let invoice = document.querySelector('.invoice').appendChild(ptag)
- 
     invoice.innerHTML = dollar.format(`${totalbill}`);
     invoice.style.color = '#fff';
 
 }
 
 checkbill.addEventListener('click', () =>{
+
     totabillresturant()
 })
- 
-let perperson = document.getElementById('perpersonbill');
-let i = 0;
 
-increment.addEventListener('click', () =>{
- 
-   totabillresturant(totalbill)
-    
-      i++;
-      if(i<1){
-       return false
-      }
- 
-      perperson.innerHTML = i;
-    console.log(i);
+let i = 0;
+let increment = document.getElementById('increment');
+let decrement = document.getElementById('decrement');
+let billperpeople = document.querySelector('#perPerson')
+
+
+function updatebill(){
+  let divide = (totalbill / i)
+  billperpeople.innerHTML = dollar.format(divide);
+}
+
+increment.addEventListener('click', function(){
+
+    if(i >= Number.MAX_VALUE){
+        i = 0;
+    }
+    i++
+    perperson.innerHTML = i;
+    updatebill();
 })
 
-decrement.addEventListener('click', function(e){
-   totabillresturant()
-    if(i<1){
-        return true;
+decrement.addEventListener('click', function(){
+    if( i >=  Number.MAX_VALUE){
+        i = 0
     }
-    let decrement_price = totabillresturant.total - perperson;
-    perperson.innerHTML =   i--;
-    console.log(decrement_price)
+    i--
+    perperson.innerHTML = i;
+    updatebill();
 })
